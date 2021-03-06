864af02593b7e67a473b24ef2427cc93c8a120a2
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-06 17:43:13 +0000

    toggle has been added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const navToggle=document.querySelector('.nav-toggle')
 const linksContainer=document.querySelector('.links-container')
 const links=document.querySelector('.links')
 
+//activate toggle button
 navToggle.addEventListener('click',function(){
     linksContainer.classList.toggle('show-links')
-})
+}) 
+
+//fiexed NavBar
