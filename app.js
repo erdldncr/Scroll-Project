ade77d053d5a1542deaa49e8efb640d38b4f59c5
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-03-07 12:04:55 +0000

    scroll event has been added and button has been displayed

--- app.js
+++ app.js
@@ -6,9 +6,36 @@ const navToggle=document.querySelector('.nav-toggle')
 const linksContainer=document.querySelector('.links-container')
 const links=document.querySelector('.links')
 
+
 //activate toggle button
 navToggle.addEventListener('click',function(){
-    linksContainer.classList.toggle('show-links')
+
+    ////getBounding gives details of selected item
+const containerHeight= linksContainer.getBoundingClientRect().height
+const linksHeight=links.getBoundingClientRect().height
+
+    if(containerHeight===0){
+    linksContainer.style.height=`${linksHeight}px`
+    }else{
+        linksContainer.style.height=0;
+    }
 }) 
 
-//fiexed NavBar
+// Fixed Nav bar
+const navbar= document.getElementById('nav')
+const topLink= document.querySelector('.top-link')
+window.addEventListener('scroll',function(){
+    const scrollHeight= window.pageYOffset;
+    const navHeight=navbar.getBoundingClientRect().height
+    if(scrollHeight>navHeight){
+        navbar.classList.add('fixed-nav')
+    }else{
+        navbar.classList.remove('fixed-nav')
+    }
+
+    if(scrollHeight>500){
+        topLink.classList.add('show-link')
+    }else{
+        topLink.classList.remove('show-link')
+    }
+})
